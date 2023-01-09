781b096623fcc31f106ca7b1e3d844c8c325ff80
Your Name <hojolkitybo@gmail.com>
2023-01-09 17:34:01 -0600

    added container proptypes

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import style from './Container.module.css';
 
-export default function Container(props) {
-  return <div className={style.section}></div>;
+export default function Container({ children }) {
+  return <div className={style.section}>{children}</div>;
 }
+
+Container.propTypes = {
+  children: PropTypes.element,
+};
